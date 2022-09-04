711ae3d6569247ca8c2d223931bae6c781b3e864
Zaki Bawany <112722407+Zaki10B@users.noreply.github.com>
2022-09-04 18:24:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+
+tim = turtle.Turtle()
+
+for i in range(10):
+  tim.left(110)
+  tim.forward(100)
+  tim.right(70)
+  tim.forward(100)
+  tim.right(105)
+  tim.forward(100)
+  tim.right(70)
+  tim.forward(100)
+  i += 1
